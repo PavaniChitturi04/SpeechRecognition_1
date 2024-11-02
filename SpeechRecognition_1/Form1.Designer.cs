aa9c446a7238e60ba0bc0c19bfdc8dee455f45d6
Srinivasa Rao <Srinivasa Rao@PAVANI>
2024-11-02 11:37:00 +0530

    Add project files.

---
+++ SpeechRecognition_1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SpeechRecognition_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LstCommands1 = new System.Windows.Forms.ListBox();
+            this.TmrSpeaking1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LstCommands1
+            // 
+            this.LstCommands1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.LstCommands1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LstCommands1.ForeColor = System.Drawing.SystemColors.Window;
+            this.LstCommands1.FormattingEnabled = true;
+            this.LstCommands1.ItemHeight = 16;
+            this.LstCommands1.Location = new System.Drawing.Point(0, 0);
+            this.LstCommands1.Name = "LstCommands1";
+            this.LstCommands1.Size = new System.Drawing.Size(535, 424);
+            this.LstCommands1.TabIndex = 0;
+            this.LstCommands1.Visible = false;
+            this.LstCommands1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // TmrSpeaking1
+            // 
+            this.TmrSpeaking1.Enabled = true;
+            this.TmrSpeaking1.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(535, 424);
+            this.Controls.Add(this.LstCommands1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstCommands;
+        private System.Windows.Forms.Timer TmrSpeaking;
+        private System.Windows.Forms.ListBox LstCommands1;
+        private System.Windows.Forms.Timer TmrSpeaking1;
+    }
+}
+
---
+++ SpeechRecognition_1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace SpeechRecognition_1
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Alyzen = new SpeechSynthesizer();
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int RecTimeOut = 0;
+        DateTime TimeNow = DateTime.Now;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(_recognizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            startlistening.SetInputToDefaultAudioDevice();
+            Choices choices = new Choices(File.ReadAllLines(@"DefaultCommands.txt"));
+
+            GrammarBuilder grammarBuilder = new GrammarBuilder(choices);
+
+            Grammar grammar = new Grammar(grammarBuilder);
+
+            startlistening.LoadGrammarAsync(grammar);
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startlistening_SpeechRecognized);
+
+
+        }
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if (speech == "Hello")
+            {
+                Alyzen.SpeakAsync("Hello, I am here");
+            }
+            if (speech == "How are you")
+            {
+                Alyzen.SpeakAsync("I am working normally");
+            }
+            if (speech == "What time is it")
+            {
+                Alyzen.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+            if (speech == "Stop talking")
+            {
+                Alyzen.SpeakAsyncCancelAll();
+                ranNum = rnd.Next(1, 2);
+                if (ranNum == 1)
+                {
+                    Alyzen.SpeakAsync("Yes sir");
+                }
+                if (ranNum == 2)
+                {
+                    Alyzen.SpeakAsync("I am sorry i will be quiet");
+                }
+            }
+            if (speech == "Stop Listening")
+            {
+                Alyzen.SpeakAsync("if you need me just ask");
+                _recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            if (speech == "Show Commands")
+            {
+                string[] commands = (File.ReadAllLines(@"DefaultCommands.txt"));
+                LstCommands1.Items.Clear();
+                LstCommands1.SelectionMode = SelectionMode.None;
+                LstCommands1.Visible = true;
+                foreach (string command in commands)
+                {
+                    LstCommands1.Items.Add(command);
+                }
+            }
+            if (speech == "Hide Commands")
+            {
+                LstCommands1.Visible = false;
+            }
+        }
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+
+            if (speech == "Wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                Alyzen.SpeakAsync("Yes, I am here");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void TmrSpeaking1_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10)
+            {
+                _recognizer.RecognizeAsyncCancel();
+
+            }
+            else if (RecTimeOut == 11)
+            {
+                TmrSpeaking1.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
